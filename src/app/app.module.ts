d3c36a3d6b51ed5299f57e1264527f5e627bb48f
Pierre BONNET <gogs@fake.local>
2020-01-22 15:04:30 +0100

    Création pokemon debut

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,6 +6,11 @@ import { AppComponent } from './app.component';
 import { GenerationsComponent } from './composants/generations/generations.component';
 import { PokemonsComponent } from './composants/pokemons/pokemons.component';
 
+import {LOCALE_ID} from '@angular/core';
+import {registerLocaleData} from '@angular/common';
+import localeFR from '@angular/common/locales/fr';
+registerLocaleData(localeFR);
+
 @NgModule({
   declarations: [
     AppComponent,
@@ -16,7 +21,7 @@ import { PokemonsComponent } from './composants/pokemons/pokemons.component';
     BrowserModule,
     AppRoutingModule
   ],
-  providers: [],
+  providers: [{provide: LOCALE_ID, useValue: 'fr'}],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
--- src/app/composants/pokemons/pokemons.component.ts
+++ src/app/composants/pokemons/pokemons.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
 
 @Component({
   selector: 'app-pokemons',
@@ -7,7 +7,69 @@ import { Component, OnInit } from '@angular/core';
 })
 export class PokemonsComponent implements OnInit {
 
-  constructor() { }
+  pokemons = [{
+    id: 1,
+    nom: 'Bulbizarre',
+    categorie: 'graine',
+    type1: 'Plante',
+    type2: 'Poison',
+    taille: 0.7,
+    poids: 6.9,
+  },
+
+    {
+      id: 2,
+      nom: 'Herbizarre',
+      categorie: 'graine',
+      type1: 'Plante',
+      type2: 'Poison',
+      taille: 1.0,
+      poids: 13.0,
+    },
+
+    {
+      id: 3,
+      nom: 'Florizarre',
+      categorie: 'graine',
+      type1: 'Plante',
+      type2: 'Poison',
+      taille: 2.0,
+      poids: 100.0,
+    },
+
+    {
+      id: 4,
+      nom: 'Salameche',
+      categorie: 'lézard',
+      type1: 'Feu',
+      type2: '',
+      taille: 0.6,
+      poids: 8.5,
+    },
+
+    {
+      id: 5,
+      nom: 'Reptincel',
+      categorie: 'flamme',
+      type1: 'Feu',
+      type2: '',
+      taille: 1.1,
+      poids: 19.0,
+    },
+
+    {
+      id: 6,
+      nom: 'Dracaufeu',
+      categorie: 'flamme',
+      type1: 'Feu',
+      type2: '',
+      taille: 1.7,
+      poids: 90.5,
+    }];
+
+
+  constructor() {
+  }
 
 
   ngOnInit() {
