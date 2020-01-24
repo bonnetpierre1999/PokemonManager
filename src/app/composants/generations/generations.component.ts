7eabc878f0cb72a0e752dd69c3381f8219134ffd
Pierre BONNET <gogs@fake.local>
2020-01-24 14:25:00 +0100

    Ajout Header3

--- src/app/composants/generations/generations.component.ts
+++ src/app/composants/generations/generations.component.ts
@@ -8,7 +8,7 @@ import { Component, OnInit } from '@angular/core';
 export class GenerationsComponent implements OnInit {
 
   // tslint:disable-next-line:max-line-length
-  private tabGenerations = ['Génération I - 151 Pokemon', 'Génération II - 100 Pokemon', 'Génération III - 135 Pokemon', 'Génération IV - 107 Pokemon',
+  private tabGenerations = ['Toutes les générations', 'Génération I - 151 Pokemon', 'Génération II - 100 Pokemon', 'Génération III - 135 Pokemon', 'Génération IV - 107 Pokemon',
     // tslint:disable-next-line:max-line-length
                             'Génération V - 156 Pokemon', 'Génération VI - 72 Pokemon', 'Génération VII - 88 Pokemon', 'Génération VIII - 81 Pokemon'];
 
